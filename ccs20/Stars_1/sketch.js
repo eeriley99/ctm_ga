a626c6f712c217b4a01c68b04539d8195d0aea35
Erin E. Riley <53475374+eeriley99@users.noreply.github.com>
2020-04-30 16:15:23 -0400

    Add files via upload

---
+++ ccs20/Stars_1/sketch.js
@@ -0,0 +1,46 @@
+function setup() {
+  createCanvas(400, 400);
+  frameRate(7)
+}
+
+function draw() {
+  background(250,0,150);
+  star(width/2, width/2, 3, 7, 5);
+  starsFall(400);
+}
+
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+function randStar(num_stars){
+  for(let i = 0; i <=num_stars; i++){
+    let x = random(0,400);
+    let y = random(0,400);
+    let r = random(0,255);
+    let b = random(0,255);
+    let g = random(0,255);
+    let rand = random(0.5,2.5);
+    star(x, y, 60, 140, 5);
+    fill(r,g,b);
+  }
+}
+function starsFall(num_stars){
+  for(let i = 0; i <=num_stars; i+=10){
+    let x = random(0,400);
+    let y = random(0,400);
+    star(x, y, 3, 7, 5);
+    fill(255,255,255);
+  }
+}
